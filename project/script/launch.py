25ae6e00da42d51f7245e5fad6b25a7d67fea2bd
Maksim Kazadaev <kazadaevms@yandex.ru>
2023-04-05 14:58:49 +0300

    Solve paths issues

--- project/script/launch.py
+++ project/script/launch.py
@@ -1,13 +1,13 @@
 import sys
-import os
 import yaml
 import subprocess
+import shutil
 from pathlib import Path
 
 
 def execute_command(cmd: str, working_directory: Path):
-    print(f'Execute {cmd} in {working_directory}')
-    result = subprocess.run(cmd, cwd=str(working_directory), shell=True, stdout=sys.stdout, stderr=sys.stderr)
+    print(f'Execute {cmd}')
+    result = subprocess.run(cmd, cwd=working_directory, shell=True, stdout=sys.stdout, stderr=sys.stderr)
     print(f'Return code: {result.returncode}')
 
 
@@ -15,32 +15,52 @@ def main():
     # Read config
     with open('config.yaml') as f:
         config = yaml.safe_load(f)
-    binary_path = config['binary_path']
+    project_path = Path(config['project_path'])
+    binary_path = project_path / 'bin/H64.EXE'
     run_file = config['run_file']
 
     # Check paths
-    assert Path(binary_path).exists()  # binary exists
+    assert project_path.exists()  # binary exists
     assert Path.cwd().name == 'script'  # current folder is scripts
 
     runs_folder = Path.cwd().parent / 'runs'
     run_path = (runs_folder / run_file).absolute()
     assert run_path.exists()  # run_file exists in runs folder
 
+    styles_folder = Path.cwd().parent / 'styles'
+    assert styles_folder.exists()
+
+    styles_file = f'{run_file}.pvsm'
+    style_path = styles_folder / styles_file
+    assert style_path.exists()
+
     output_directory = (Path.cwd().parent / 'output').absolute()
     output_directory.mkdir(exist_ok=True)
     assert output_directory.exists()
-    # delete output/*
+
+    # Replace .pvsm file paths
+    with open(style_path, 'r') as f:
+        content = f.read()
+    content = content.replace(rf'F:\SIMULATIONS\TESTING\{run_file}', str(runs_folder))
+    with open(runs_folder / styles_file, 'w') as f:
+        f.write(content)
+
+    # Delete output/*
     for file in output_directory.iterdir():
         file.unlink()
-    # delete runs/* which does not end with .run and .pvsm
-    for file in runs_folder.iterdir():
-        if not file.name.endswith('.run') and not file.name.endswith('.pvsm'):
-            file.unlink()
-    execute_command(f'"{binary_path}" "{run_path}"', working_directory=output_directory)
 
+    # Delete runs/* which does not end with .run and .pvsm
+    for file in runs_folder.iterdir():
+        if '.' in file.name and not file.name.endswith('.pvsm'):
+            file.unlink()
+
+    # Execute simulation
+    execute_command(f'"{binary_path}" "{run_path}"', working_directory=runs_folder)
+
+    # Move csv files to output directory
     for file in runs_folder.iterdir():
         if 'CSV' in file.name:
-            file.rename(output_directory / file.name)
+            shutil.copy(file, output_directory / file.name)
 
 
 if __name__ == '__main__':
